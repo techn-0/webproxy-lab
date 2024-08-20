23771ae874bf45ea0d719a1f4817d4fd2d09a4a8
Ubuntu <ubuntu@ip-172-31-7-249.ap-northeast-2.compute.internal>
2024-08-20 04:44:18 +0000

    시작

---
+++ echo/echoclient.c
@@ -0,0 +1,14 @@
+#include "csapp.h"
+
+int main(int argc, char **argv)
+{
+    int chlientf;
+    char *host, *port, buf[MAXLINE];
+    rio_t rio;
+
+    if (argc != 3)
+    {
+        /* code */
+    }
+    
+}
