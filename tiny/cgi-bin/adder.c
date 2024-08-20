bc141a9a37158e2c1f1f99665389bc383fcb13ca
Alisser <gnlrjswjd@naver.com>
2024-08-20 19:46:56 +0900

    a11.10

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,36 +4,38 @@
 /* $begin adder */
 #include "csapp.h"
 
-int main(void) 
+int main(void)
 {
-  char *buf, *p, *method;
+  char *buf, *p;
   char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
-  int n1=0, n2=0;
+  int n1 = 0, n2 = 0;
 
-  /* Extract the two arguments */
-  if ((buf = getenv("QUERY_STRING")) != NULL) {
+  if ((buf = getenv("QUERY_STRING")) != NULL)
+  {
     p = strchr(buf, '&');
     *p = '\0';
     strcpy(arg1, buf);
-    strcpy(arg2, p+1);
-    n1 = atoi(arg1);
-    n2 = atoi(arg2);
+    strcpy(arg2, p + 1);
+    n1 = atoi(arg1 + 5);
+    n2 = atoi(arg2 + 5);
   }
-  /* Make the response body */
+
   sprintf(content, "QUERY_STRING=%s", buf);
   sprintf(content, "Welcome to add.com: ");
-  sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+  sprintf(content, "%sTHE Internet Addition portal.\r\n<p>", content);
   sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+
+  sprintf(content, "%s<form action=\"adder\" method=\"GET\">\r\n", content);
+  sprintf(content, "%sNUM1: <input type=\"text\" name=\"num1\" value=\"%d\">\r\n", content, n1);
+  sprintf(content, "%sNUM2: <input type=\"text\" name=\"num2\" value=\"%d\">\r\n", content, n2);
+  sprintf(content, "%s<input type=\"submit\" value=\"GO!\"></form>\r\n", content);
+
   sprintf(content, "%sThanks for visiting!\r\n", content);
-  /* Generate the HTTP response */
+
   printf("Connection: close\r\n");
   printf("Content-length: %d\r\n", (int)strlen(content));
   printf("Content-type: text/html\r\n\r\n");
-
-  // method가 GET일 경우 response body 전송
-  if(strcasecmp(method, "GET") == 0) { 
-    printf("%s", content);
-  }
+  printf("%s", content);
   fflush(stdout);
 
   exit(0);
